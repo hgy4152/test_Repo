110d54df67b2ccdcb0d1edb985802ec71bf4407c
Giyeong - Heo <hgy4152@gmail.com>
2026-02-23 17:26:12 +0900

    hgy
    발표자료

---
+++ ConsoleApp7/Party_Selector_No_Function.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+// 함수 없이
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml.Linq;
+
+// 값을 받을 배열 생성
+// 유저 개별로 받은 정보를 다뤄야 하기 때문에
+// 한 인덱스에 여러 정보들을 한번에 저장하기 위해 가변배열로 지정
+// 최종 멤버의 정보를 받을 빈 배열도 지정
+// 배열을 안쓰면 요소 하나하나 전부 다 따로 변수에 재할당 하면서 for문을 실행해줘야함
+// 전체 값을 재설정하는게 가능은 하지만 데이터가 늘면 늘수록 메모리 소모가 극심해짐.
+
+
+string[][] member = new string[5][];
+string[][] partyMember = new string[5][];
+
+// 정보를 string으로 받음
+// readline 이나 다른 곳으로부터 정보를 받았다고 가정함
+partyMember[0] = new string[] { "루시드", "250", "180", "true" };
+partyMember[1] = new string[] { "윌", "89", "95", "false" };
+partyMember[2] = new string[] { "데미안", "200", "210", "true" };
+partyMember[3] = new string[] { "세렌", "175", "160", "true" };
+partyMember[4] = new string[] { "칼로스", "310", "300", "false" };
+
+//입장 조건 체크 변수
+bool isLevel = false;
+bool isScore = false;
+bool isQuest = false;
+
+// 최종 파티원 수 카운트
+int count = 0;
+
+Console.WriteLine("=== 던전: 검은 마법사의 성 ===");
+Console.WriteLine();
+Console.WriteLine("** 입장 조건 **");
+Console.WriteLine(
+    "1. 레벨 200 이상" +
+    "\n2. 장비 점수 150 이상" +
+    "\n3. 사전 퀘스트 완료");
+Console.WriteLine();
+Console.WriteLine("==== 지원 목록 ====");
+
+// 신청받은 파티원 수에 따라 for문 작동
+for (int i = 0; i < partyMember.Length; i++)
+{
+    // 한 멤버의 조건 판단이 끝날 때 마다 초기화
+    isLevel = false;
+    isScore = false;
+    isQuest = false;
+
+    // 파티원 개별정보를 조사
+    for (int j = 0; j < partyMember[i].Length; j++)
+    {
+        // 정보 출력
+        Console.Write($"{partyMember[i][j]}\t");
+
+        // 받은 데이터 양식이 고정이기 때문에 동일한 위치에 해당 정보가 들어있음
+        // 인덱스로 분류해서 조건 판단
+        // 형변환을 전부 해주면서 해야한다는 불편함이 있음.
+        switch (j)
+        {
+            case 1:
+                if (int.Parse(partyMember[i][j]) >= 200) { isLevel = true; } //레벨
+                break;
+            case 2:
+                if (int.Parse(partyMember[i][j]) >= 150) { isScore = true; } //장비점수
+                break;
+            case 3:
+                if (partyMember[i][j] == "true") { isQuest = true; } // 퀘스트 클리어 여부
+                break;
+        }
+    }
+
+    // 개별정보 조사가 끝났을 때 조건 만족 시 최종멤버 배열에 넣어준 후 카운트
+    if (isLevel && isQuest && isScore)
+    {
+
+        member[count] = partyMember[i];
+        count++;
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("==== 최종 파티 ====");
+
+// 최종멤버 정보를 받은 배열에서 이름만 추출해서 출력
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine(member[i][0]);
+}
+
+Console.WriteLine($"총 인원 수: {count}");
---
+++ hgy/Party_Selector.cs
@@ -0,0 +1,153 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml.Linq;
+
+// 함수 이용
+
+Console.WriteLine("=== 던전: 검은 마법사의 성 ===");
+Console.WriteLine();
+Console.WriteLine("** 입장 조건 **");
+Console.WriteLine(
+    "1. 레벨 200 이상" +
+    "\n2. 장비 점수 150 이상" +
+    "\n3. 사전 퀘스트 완료");
+Console.WriteLine();
+
+
+// 이름만 넘겨주면 되기 때문에 StringBuilder 이용
+// 모든 정보를 출력하고 싶으면 위의 방식대로 하면 됌
+StringBuilder sb = new StringBuilder();
+
+
+// 입장조건 확인 변수
+bool isEntry = false;
+
+// 파티원 수 체크
+int count = 0;
+    
+// 멤버 정보 체크하는 함수
+void MemberCheck(string name, int level = 1, int score = 1, bool isQuest = false )
+{
+    // 체크 조건 초기화
+    isEntry = false;
+
+    // 정보 출력
+    Console.WriteLine($"{name}");
+        
+    // 받은 정보로 유저 상태 체크 및 정보 출력
+    ConditionCheck(level, score, isQuest);
+    count += MemberCount(name);
+        
+
+
+}
+
+// 입장 조건 체크. MemberCheck()함수 안에서 실행
+void ConditionCheck(int level = 1, int score = 1, bool isQuest = false)
+{
+
+    // 받은 매개변수를 토대로 입장 조건 플래그 설정
+
+
+
+    // 플래그에 따라 정보 및 스크립트 출력
+    // 얻은 bool 값을 입장 확인 함수로 넘겨줌
+    // 여기서 바로 체크를 해도 되지만 기능 분리를 해서 가독성을 높임
+    isEntry = EntryCondition(LevelCheck(level), ScoreCheck(score), QuestCheck(isQuest));
+
+
+    Console.WriteLine(isEntry ? "입장 가능" : "입장 불가능");
+    Console.WriteLine("-----------------------");
+}
+
+// 레벨 체크
+bool LevelCheck(int level)
+{
+    bool isLevel = level >= 200;
+    Console.WriteLine(isLevel ? $"레벨: {level}" : $"레벨: {level} !! 레벨이 부족합니다(입장레벨 200이상) !!");
+
+    return isLevel;
+
+}
+
+// 장비 점수 체크
+bool ScoreCheck(int score)
+{
+    bool isScore = score >= 150;
+    Console.WriteLine(isScore ? $"장비점수: {score}" : $"장비 점수: {score} !! 장비 점수가 부족합니다(장비점수 150점 이상 !!)" );
+
+    return isScore;
+
+}
+
+// 퀘스트 체크
+bool QuestCheck(bool quest)
+{
+
+    Console.WriteLine(quest ? $"선행퀘스트 완료" : "!! 선행퀘스트를 완료해주세요 !!");
+
+    return quest;
+
+}
+
+
+// 입장 확인 및 플래그 설정
+// 기능 분리를 통해 향후 추가되는 조건 대비나 가독성을 높이기 위함. << #1
+bool EntryCondition(bool islevel, bool isScore, bool isQuest)
+{
+
+    return isScore && islevel && isQuest;
+
+}
+
+// 입장 가능한 인원인지 계산하는 함수
+// 이름도 같이받아서 명단에 추가함
+int MemberCount(string name)
+{
+    // 파티원 명단에 추가
+    if (isEntry)
+    {
+        sb.AppendLine(name);
+
+    }
+
+    // 멤버 카운트
+    return isEntry ? 1 : 0;
+
+}
+
+
+// 최종 파티원 목록 출력
+void LastPartyMember()
+{
+    Console.WriteLine("==== 최종 파티 ====");
+    Console.Write(sb); // 줄바꿈으로 분리되기 때문에 write 사용
+    Console.WriteLine($"총 인원 수: {count}");
+}
+
+
+// 위처럼 배열로 받는게 편하지만 함수에서 특정 요소들을 편하게 활용하기 위해서
+// 각 타입별로 분리해서 작성. 실제로는 입력값이 어떻게 들어올 지는 미지수라 어느게 낫다 하기 뭐함. <<#2
+// 다만 매개변수를 활용하면 새로운 정보칸이 생겼을 때 정보 정렬이 편하지만
+// 배열로 하면 수정이 불가하기에 새로 싹 리뉴얼 해야하는 단점이 명확함.
+Console.WriteLine("==== 지원 목록 ====");
+MemberCheck("루시드", 250, 180, true);
+Console.WriteLine();
+    
+MemberCheck("윌", 89, 95, false);
+Console.WriteLine();
+    
+MemberCheck("데미안", 200, 210, true);
+Console.WriteLine();
+    
+MemberCheck("세렌", 175, 160, true);
+Console.WriteLine();
+    
+MemberCheck("칼로스", 310, 300, false);
+Console.WriteLine();
+
+LastPartyMember();
+
+
+
